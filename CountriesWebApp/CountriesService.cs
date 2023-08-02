aacd44f602709f7ea9465ce30bcf9859606c0de1
Virgil Danciu <vdanci@softserveinc.com>
2023-08-02 15:45:04 +0300

    UC#1 Integration

--- CountriesWebApp/CountriesService.cs
+++ CountriesWebApp/CountriesService.cs
@@ -1,7 +1,4 @@
 ﻿using CountriesWebApp.Model;
-using System.Diagnostics.Metrics;
-using System.Linq;
-using System.Text.Json;
 
 namespace CountriesWebApp
 {
@@ -17,67 +14,11 @@ namespace CountriesWebApp
         public async Task<IEnumerable<Country>> GetCountriesAsync(string country, int? population, string countrySort, int? numberOfRecords)
         {
             var countries = await _apiClient.GetAllCountriesAsync();
-            
-            countries = FilterCountries(countries, country);
-            countries = FilterByPopulation(countries, population);
-            countries = SortCountries(countries, countrySort);
-            countries = GetFirstNRecords(countries, numberOfRecords);
 
-            return countries;
-        }
-
-        private static IEnumerable<Country> FilterCountries(IEnumerable<Country> countries, string country)
-        {
-            if (string.IsNullOrEmpty(country))
-            {
-                return countries; // If search is empty or null, return all countries
-            }
-
-            // Perform the filtering using LINQ
-            var filteredCountries = countries.Where(c =>
-                c.Name.Common.Contains(country, StringComparison.OrdinalIgnoreCase)
-            );
-
-            return filteredCountries;
-        }
-
-        private static IEnumerable<Country> FilterByPopulation(IEnumerable<Country> countries, int? millions)
-        {
-            if (!millions.HasValue)
-            {
-                return countries;
-            }
-
-            return countries.Where(c => c.Population < millions.Value * 1000000);
-        }
-
-        private static IEnumerable<Country> SortCountries(IEnumerable<Country> countries, string sortDirection)
-        {
-            if (string.IsNullOrEmpty(sortDirection))
-            {
-                return countries;
-            }
-
-            if (sortDirection.ToLower() == "ascend")
-            {
-                return countries.OrderBy(country => country.Name.Common);
-            }
-            else if (sortDirection.ToLower() == "descend")
-            {
-                return countries.OrderByDescending(country => country.Name.Common);
-            }
-
-            return countries;
-        }
-
-        private static IEnumerable<Country> GetFirstNRecords(IEnumerable<Country> countries, int? numberOfRecords)
-        {
-            if (!numberOfRecords.HasValue || numberOfRecords.Value <= 0)
-            {
-                return countries;
-            }
-
-            return countries.Take(numberOfRecords.Value);
+            return countries.FilterCountries(country)
+                            .FilterByPopulation(population)
+                            .SortCountries(countrySort)
+                            .GetFirstNRecords(numberOfRecords);
         }
     }
 }
---
+++ CountriesWebApp/CountryFilter.cs
@@ -0,0 +1,61 @@
+﻿using CountriesWebApp.Model;
+
+namespace CountriesWebApp
+{
+    public static class CountryFilter
+    {
+        public static IEnumerable<Country> FilterCountries(this IEnumerable<Country> countries, string country)
+        {
+            if (string.IsNullOrEmpty(country))
+            {
+                return countries; // If search is empty or null, return all countries
+            }
+
+            // Perform the filtering using LINQ
+            var filteredCountries = countries.Where(c =>
+                c.Name.Common.Contains(country, StringComparison.OrdinalIgnoreCase)
+            );
+
+            return filteredCountries;
+        }
+
+        public static IEnumerable<Country> FilterByPopulation(this IEnumerable<Country> countries, int? millions)
+        {
+            if (!millions.HasValue)
+            {
+                return countries;
+            }
+
+            return countries.Where(c => c.Population < millions.Value * 1000000);
+        }
+
+        public static IEnumerable<Country> SortCountries(this IEnumerable<Country> countries, string sortDirection)
+        {
+            if (string.IsNullOrEmpty(sortDirection))
+            {
+                return countries;
+            }
+
+            if (sortDirection.ToLower() == "ascend")
+            {
+                return countries.OrderBy(country => country.Name.Common);
+            }
+            else if (sortDirection.ToLower() == "descend")
+            {
+                return countries.OrderByDescending(country => country.Name.Common);
+            }
+
+            return countries;
+        }
+
+        public static IEnumerable<Country> GetFirstNRecords(this IEnumerable<Country> countries, int? numberOfRecords)
+        {
+            if (!numberOfRecords.HasValue || numberOfRecords.Value <= 0)
+            {
+                return countries;
+            }
+
+            return countries.Take(numberOfRecords.Value);
+        }
+    }
+}
